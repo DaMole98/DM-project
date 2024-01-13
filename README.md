db8702efcc060c2f12220eb1ca2284fb103382dc
francesco boldrin <francesco.boldrin@studenti.unitn.it>
2024-01-13 15:17:22 +0100

    some adjustment

--- README.md
+++ README.md
@@ -13,6 +13,45 @@ The data is available in the folder `data`.
 
 The code is available in the folder `src`.
 
-## Report
+## Project structure
 
-The report is available in the folder `report`.
+The project structure is the following:
+
+```
+.
+??? data
+?   ??? data_description.txt
+?   ??? sample_submission.csv
+?   ??? test.csv
+?   ??? train.csv
+??? DM_project.pdf
+??? README.md
+??? src
+    ??? data_preparation.py
+    ??? data_preparation.pyc
+    ??? __init__.py
+    ??? __init__.pyc
+    ??? main.py
+    ??? main.pyc
+    ??? model.py
+    ??? model.pyc
+    ??? plot.py
+    ??? plot.pyc
+    ??? preprocessing.py
+    ??? preprocessing.pyc
+    ??? __pycache__
+    ?   ??? data_preparation.cpython-36.pyc
+    ?   ??? __init__.cpython-36.pyc
+    ?   ??? main.cpython-36.pyc
+    ?   ??? model.cpython-36.pyc
+    ?   ??? plot.cpython-36.pyc
+    ?   ??? preprocessing.cpython-36.pyc
+    ?   ??? utils.cpython-36.pyc
+    ??? utils.py
+``` 
+
+## How to run the code
+
+To run the code, you need to run the file `main.py` in the folder `src`. The code is written in Python 3.6. The required packages are listed in the file `requirements.txt`. To install them, you can run the following command:
+
+```
--- src/Solver_Programs/plotVisualizer.py
+++ src/Solver_Programs/plotVisualizer.py
@@ -31,3 +31,39 @@ def plotUtilityMatrix(utility_matrix, drivers, title):
     ax.set_yticklabels([])
 
     plt.savefig(f"{output_path}{title}.png")
+
+def plotUtilityMatrix2(utility_matrix, drivers, title):
+    """
+    :param utility_matrix: utility matrix with content based approach
+    :type utility_matrix: dict
+    :param drivers: list of drivers
+    :param title: title of the plot
+    :return: None
+    """
+    cmap = ListedColormap(['lightblue', 'green'])
+
+    fig, ax = plt.subplots(figsize=(20, 15))
+    ax.set_aspect('equal')
+
+    for i, row in enumerate(utility_matrix):
+        driver_name = drivers[i]
+        for j, value in enumerate(row):
+            if value is not None:
+                ax.add_patch(plt.Rectangle((j, -i - 1), 1, 1, color=cmap(value > 0), alpha=0.7))
+                ax.text(j + 0.5, -i - 0.5, f'{value:.2f}', color='black', ha='center', va='center', fontsize=8)
+            else:
+                ax.add_patch(plt.Rectangle((j, -i - 1), 1, 1, color='lightblue', alpha=0.7))
+
+        ax.text(-0.5, -i - 0.5, f'{driver_name}', color='black', ha='right', va='center', fontsize=10,
+                fontweight='bold')
+
+    ax.set_title(title, fontsize=20)
+
+    ax.set_xlim(0, len(utility_matrix[0])+1)
+
+    ax.set_xticks(np.arange(len(utility_matrix[0])) + 0.5)
+    ax.set_yticks(-np.arange(len(utility_matrix)+1) - 0.5)
+    ax.set_xticklabels([f's{i}' for i in range(len(utility_matrix[0]))])
+    ax.set_yticklabels([])
+
+    plt.savefig(f"{output_path}{title}.png")
